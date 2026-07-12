99f942146d9ccb5db2a229d5ae1fdffc0b5178d4
Jordan <officialchicagoshots@gmail.com>
2026-07-11 19:26:48 -0500

    fix(customization): follow latest app cache version

--- app/js/customization.js
+++ app/js/customization.js
@@ -1,4 +1,4 @@
-import { currentTenantId, session } from "./store.js?v=phantom-live-20260711-194";
+import { currentTenantId, session } from "./store.js?v=phantom-live-20260711-195";
 
 let activeConfiguration = null;
 let activeEntitlements = null;
